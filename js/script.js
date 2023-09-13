3b388668e34fae06b733819d3cef95c180efd17a
Oleksandr Krynytskyi <neomancer2@gmail.com>
2023-09-13 19:56:11 +0300

    Callback form

--- js/script.js
+++ js/script.js
@@ -14,3 +14,30 @@ $('.mobile-menu__close, .modal-bcg').on('click', function () {
         $('.mobile-menu').removeClass('active');
     }, 500)
 })
+
+let input_value = $('.callback input');
+
+$('.callback button').on('click', function () {
+    const re = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+
+    if (!re.test(input_value.val())) {
+        $(input_value).addClass('error');
+    } else {
+        // send data to server
+
+        Swal.fire({
+            position: 'top-end',
+            icon: 'success',
+            title: 'Дякуємо за підписку',
+            showConfirmButton: false,
+            timer: 3000
+        })
+        input_value.val('');
+    }
+})
+
+$(input_value).on('focus', function () {
+    if ($(this).hasClass('error')) {
+        $(this).removeClass('error')
+    }
+})
